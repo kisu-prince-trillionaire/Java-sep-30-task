e4f3226ac4f8dc917e13489441b6a661b3b2bbaf
VISHAL KUMAR - Boolean Pirate <vishalkrnda@gmail.com>
2022-10-01 14:42:20 +0530

    fc1

--- src/com/exam/resources/MyConfiguration.java
+++ src/com/exam/resources/MyConfiguration.java
@@ -1,9 +1,24 @@
 package com.exam.resources;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.exam.components.Exam;
+import com.feedbean.FeedExamMap;
 
 @Configuration
 public class MyConfiguration {
 	@Bean
+	public Exam createExam() {
+		Exam exam = new Exam();
+		exam.seteId(1111);
+		exam.setName("Physics");
+		exam.setMarks(98);
+		FeedExamMap fem = new FeedExamMap();
+		exam.setSubject((List<String>) fem);
+		return exam;
+	}
 	
 }
